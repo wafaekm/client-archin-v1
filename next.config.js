7aa1b811ef49aad152cbdd6890bfb7ee76beb361
wafae <wafae@MacBook-Pro-de-wafae.local>
2026-01-17 14:23:47 +0100

    remove videos

--- next.config.js
+++ next.config.js
@@ -1,32 +1,9 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-
-// IMPORTANT: GitHub Pages project site => https://<user>.github.io/<repoName>/
-const repoName = "client-archin-v1";
-
 const nextConfig = {
   output: "export",
-  reactStrictMode: false,
-
-  sassOptions: {
-    includePaths: [path.join(__dirname, "css")],
-  },
-
-  trailingSlash: true,
-
-  devIndicators: {
-    buildActivity: false,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-
-  // GitHub Pages base path
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-
   images: { unoptimized: true },
+  trailingSlash: true,
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
