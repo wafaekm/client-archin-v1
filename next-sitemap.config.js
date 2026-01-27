0bf1a44c94afbfe620ecbd10d8d3fa12527f9af6
wafae <wafae@MacBook-Pro-de-wafae.local>
2026-01-27 13:34:55 +0100

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://rohalmokawlah.com",
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    // Si tu as des routes à exclure:
+    exclude: ["/404", "/_not-found"],
+  };
+  
